7116f72ccc876fc62bf68caeed95d0de100accb4
seonghee-lee <heeracle@naver.com>
2022-07-02 19:07:41 +0900

    0630

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,21 @@
-
+import java.io.*;
+import java.util.*;
 class Main{
-    public static void main(String[] args) {
+    private static int a, b;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str="";
+
+        //EOF처리
+        while((str=br.readLine()) != null){
+            StringTokenizer st = new StringTokenizer(str);
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+
+            System.out.println(a+b);
+        }
     }
 }
+
